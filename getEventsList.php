2e5f9594fc37a76128118546ea24919751719801
BARKER, Kyle (Student) <kb477@students.waikato.ac.nz>
2025-03-02 13:15:59 +1300

    Removed unneeded code

--- getEventsList.php
+++ getEventsList.php
@@ -1,33 +1,3 @@
-<!-- <?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-
-require_once('connectdb.php');
-
-// if ($con) {
-//     die("Connection failed: " . $cnn->connect_error);
-// }
-
-
-
-
-$query = "SELECT name FROM events";
-
-$result = $con->query($query);
-
-//$events = [];
-// while ($row = $result->fetch_assoc()) {
-//     $events[] = $row;
-// }
-
-echo json_encode($events);
-
-// $con->close();
-
-?> -->
-
 <?php
 
 error_reporting(E_ALL);
@@ -36,9 +6,9 @@ ini_set('display_errors', 1);
 include 'connectdb.php'; // Include the PDO connection
 
 $sql = "SELECT name FROM events";
-$stmt = $con->query($sql); // Execute query
+$result = $con->query($sql); // Execute query
 
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     echo $row["name"] . "<br>";
 }
 ?>
