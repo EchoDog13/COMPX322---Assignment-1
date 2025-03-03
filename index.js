6ed817b8c031b262b1a113d58aafa75952fefc5f
BARKER, Kyle (Student) <kb477@students.waikato.ac.nz>
2025-03-03 13:39:24 +1300

    removing test code

--- index.js
+++ index.js
@@ -6,13 +6,6 @@ function getEventsList() {
       // Check if request was successful
       let events = JSON.parse(asyncRequest.responseText); // Parse JSON
 
-      //   let content = document.getElementById("testArea");
-      //   content.innerHTML = data.map((event) => `<p>${event.name}</p>`).join(""); // Display event names
-
-      //   console.log(data); // Log the JSON data
-      // } else {
-      //   console.error("Error: " + asyncRequest.status);
-
       events.array.forEach((element) => {
         let eventElement = document.createElement("div");
         eventElement.textContent = element.name;
