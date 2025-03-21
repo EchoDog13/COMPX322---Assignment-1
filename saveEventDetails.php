96b2c4e837611b1345edf08bdeba77b49618e7c0
BARKER, Kyle (Student) <kb477@students.waikato.ac.nz>
2025-03-22 12:27:53 +1300

    ts

--- saveEventDetails.php
+++ saveEventDetails.php
@@ -25,8 +25,8 @@ try {
         //UPDATE into db at ID
         $sql = "UPDATE events SET name = '$name', lat_lon = '$lat_long', cost = '$cost', month = '$month', day = '$day', time = '$time', location = '$location', tagged = '$tagged' WHERE id = $id";
 
-
         $con->query($sql);
+        echo ($sql);
     }
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
